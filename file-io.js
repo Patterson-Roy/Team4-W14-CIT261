85820d7e832fe3260fa217968e66d1fd4f8320e0
Patterson-Roy <roy.patterson34@gmail.com>
2014-01-30 16:10:53 -0700

    testing code for firebase

--- file-io.js
+++ file-io.js
@@ -1,36 +1,40 @@
+var m_sUserID = "secret user";
 var addRecords = function (event) {
     event.preventDefault();
     output.textContent = "";
-    var recKey="";
 
     var data = {}; // javascript object
     
     var xhtmlreq = new XMLHttpRequest();
-    var name = document.getElementById("name").value;
-    var address = document.getElementById("address").value;
+    var rate = document.getElementById("rate").value;
+    var principal = document.getElementById("principal").value;
+    var sWhatIf = document.getElementById("what-if").value;
     
-    if((name !== typeof "undefined" && name !== "" && name !== null) &&
-       (address !== typeof "undefined" && address !== "" && address !== null)){
-        data.name = name;
-        data.address = address;
-        recKey=name+address;
-        xhtmlreq.open("PUT","https://blistering-fire-7540.firebaseio.com/testdata.json",true);
+    if((sWhatIf !== typeof "undefined" && sWhatIf !== "" && sWhatIf !== null)){
+        data.principal = principal;
+        data.rate = rate;
+        
+        xhtmlreq.open("PUT","https://blistering-fire-7540.firebaseio.com/" + m_sUserID + "/" + sWhatIf + ".json",true);
         xhtmlreq.setRequestHeader("Content-Type", "application/json");
         xhtmlreq.send(JSON.stringify(data));
     }else{
         output.textContent = "record not added fields are empty";
     }
     
-    document.getElementById("name").value="";
-    document.getElementById("address").value="";
+//    document.getElementById("principal").value="";
+//    document.getElementById("rate").value="";
+    document.getElementById("what-if").value="";
 }
 
 
 var getRecords = function (event) {
     event.preventDefault();
+    
+    var sWhatIf = document.getElementById("what-if").value;
+    
     document.getElementById("output").textContent="";
     var xhtmlreq= new XMLHttpRequest();
-    xhtmlreq.open("GET","https://blistering-fire-7540.firebaseio.com/testdata.json");
+    xhtmlreq.open("GET","https://blistering-fire-7540.firebaseio.com/" + m_sUserID + "/"+ sWhatIf + ".json");
     
     xhtmlreq.send(null);
     xhtmlreq.onreadystatechange = function(){
@@ -39,7 +43,33 @@ var getRecords = function (event) {
             var data = JSON.parse(xhtmlreq.responseText);
             var output = document.getElementById("output");
             if(data !== typeof "undefined" && data !== null){
-                output.textContent = data.name + " " + data.address;
+                output.textContent = data.principal + " " + data.rate;
+            }
+        }
+    }
+}
+
+
+var getAllRecords = function (event) {
+    event.preventDefault();
+    
+    var sWhatIf = document.getElementById("what-if").value;
+    
+    document.getElementById("output").textContent="";
+    var xhtmlreq= new XMLHttpRequest();
+    xhtmlreq.open("GET","https://blistering-fire-7540.firebaseio.com/" + m_sUserID + ".json");
+    
+    xhtmlreq.send(null);
+    xhtmlreq.onreadystatechange = function(){
+        if(xhtmlreq.readyState == 4 && xhtmlreq.status == 200){
+            console.log(xhtmlreq.responseText, xhtmlreq.responseType, xhtmlreq.responseXML);
+            var data = JSON.parse(xhtmlreq.responseText);
+            if(data !== typeof "undefined" && data !== null){
+                // data is an Object type.  The keys is an inherited function of Object that returns an array of keys to that object.
+                // we can do a forEach on the data object this way to get the parts and pieces we need for the app.
+                Object.keys(data).forEach(function (key, index) {
+                    document.getElementById("output" + index).textContent = "key: " + key + "   principal: " + data[key].principal + "   rate: " + data[key].rate;
+                });
             }
         }
     }
@@ -47,10 +77,15 @@ var getRecords = function (event) {
 
 var delRecords = function(event){
     event.preventDefault();
+    
+    var sWhatIf = document.getElementById("what-if").value;
+    
     document.getElementById("output").textContent="";
+    document.getElementById("output1").textContent="";
+    document.getElementById("output2").textContent="";
     var xhtmlreq = new XMLHttpRequest();
     var output = document.getElementById("output");
-    xhtmlreq.open("DELETE","https://blistering-fire-7540.firebaseio.com/testdata.json");
+    xhtmlreq.open("DELETE","https://blistering-fire-7540.firebaseio.com/" + m_sUserID + "/"+ sWhatIf + ".json");
     xhtmlreq.send(null);
     xhtmlreq.onreadystatechange = function(){
         if(xhtmlreq.readyState == 4 && xhtmlreq.status == 200){
@@ -61,9 +96,10 @@ var delRecords = function(event){
 }
 
 var init = function(event){
-    document.getElementById("add-records").addEventListener('click',addRecords);
-    document.getElementById("delete-records").addEventListener('click',delRecords);
-    document.getElementById("get-records").addEventListener('click',getRecords);
+    document.getElementById("add-records").addEventListener('click', addRecords);
+    document.getElementById("delete-records").addEventListener('click', delRecords);
+    document.getElementById("get-records").addEventListener('click', getRecords);
+    document.getElementById("get-all-records").addEventListener('click', getAllRecords);
 }
 
 document.addEventListener("DOMContentLoaded",init);
