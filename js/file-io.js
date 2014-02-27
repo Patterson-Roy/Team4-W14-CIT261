a765b3b215f9bffa22842ee0e2929eee0a50104f
Patterson-Roy <roy.patterson34@gmail.com>
2014-02-27 11:53:02 -0700

    code clean up and use of window.btoa (convert inbound firebase table IO) and window.atob (outbound conversion from firebase).

--- js/file-io.js
+++ js/file-io.js
@@ -32,8 +32,9 @@ function addRecords () {
         data.payment = fPayment.replace( /,/g, "" ); // strip any commas
         data.total = fTotal.replace(/,/g,""); // strip any commas
         data.totalinterest = fTotalInterest.replace( /,/g, "" ); // strip any commas;
-
-        xhtmlreq.open("PUT","https://flickering-fire-5311.firebaseio.com/" + sUserID + "/" + sScenarioName + "/" + sLoanType + ".json",true);
+        
+        // window.btoa is a base64 converter for use with firebase IO.
+        xhtmlreq.open("PUT","https://blistering-fire-7540.firebaseio.com/" + window.btoa(sUserID) + "/" + window.btoa(sScenarioName) + "/" + sLoanType + ".json",true);
         xhtmlreq.setRequestHeader("Content-Type", "application/json");
         xhtmlreq.send(JSON.stringify(data));
 
@@ -41,44 +42,10 @@ function addRecords () {
         sessionStorage.setItem(sScenarioName + "|" + sLoanType,JSON.stringify(data));
         return true;
     }catch(exception){
+        console.log(exception);
         return false;
     }
 }
-
-
-//var getRecord = function (event) {
-//    event.preventDefault();
-//    
-//    var sUserID = localStorage.getItem('nerdherdcalc-userid');
-//    if(sUserID === null || sUserID === "")
-//        return;
-//
-//    
-//    var sScenarioName = document.getElementById("scenario-name").value;
-//    var sLoanType = document.getElementById("loan-type").value;
-//    
-//    var xhtmlreq= new XMLHttpRequest();
-//    xhtmlreq.open("GET","https://flickering-fire-5311.firebaseio.com/" + sUserID + "/"+ sScenarioName + "/" + sLoanType +".json");
-//    
-//    xhtmlreq.send(null);
-//    xhtmlreq.onreadystatechange = function(){
-//        if(xhtmlreq.readyState == 4 && xhtmlreq.status == 200){
-//            var data = JSON.parse(xhtmlreq.responseText);
-//            var output = document.getElementById("output");
-//            if(typeof(data) !== "undefined" && data !== null){
-//                document.getElementById("scenario-name").value = sScenarioName;
-//                document.getElementById("loan-type").value = sLoanType; // car, home, other
-//                document.getElementById("rate").value = NumberWithCommas(parseFloat(data.rate).toFixed(3));
-//                document.getElementById("principal").value = NumberWithCommas(parseFloat(data.principal).toFixed(2));
-//                document.getElementById("periods").value = parseInt(data.term);
-//                document.getElementById("period-type").value = data.periodtype; // month, quarter, year
-//                document.getElementById("payment").value = NumberWithCommas(parseFloat(data.payment).toFixed(2));
-//                document.getElementById("total").value = NumberWithCommas(parseFloat(data.total).toFixed(2));
-//                document.getElementById("interest-total").value = NumberWithCommas(parseFloat(data.totalinterest).toFixed(2));
-//            }
-//        }
-//    }
-//}
 
 
 var getAllRecords = function (event) {
@@ -95,7 +62,8 @@ var getAllRecords = function (event) {
         var item = {};
 
         var xhtmlreq= new XMLHttpRequest();
-        xhtmlreq.open("GET","https://flickering-fire-5311.firebaseio.com/" + sUserID + ".json");
+        // window.btoa is a base64 converter for use with firebase IO.
+        xhtmlreq.open("GET","https://blistering-fire-7540.firebaseio.com/" + window.btoa(sUserID) + ".json");
 
         xhtmlreq.send(null);
         xhtmlreq.onreadystatechange = function(){
@@ -112,19 +80,23 @@ var getAllRecords = function (event) {
                             item = document.createElement("button");
                             out.appendChild(item);
 
-                            item.textContent = key + " " + key1 ;
-                            item.setAttribute("id",key + "-" + key1);
-                            item.keyValue = key + "|" + key1;
+                            // window.atob is converter from base64 back to base 16.
+                            sSessionKey = window.atob(key);
+                            
+                            item.textContent = sSessionKey + " " + key1 ;
+                            item.setAttribute("id",sSessionKey + "-" + key1);
+                            item.keyValue = sSessionKey + "|" + key1;
                             item.addEventListener('click',btnScenario);
 
                             // save data for session storage
-                            sessionStorage.setItem(key + '|' +  key1,JSON.stringify(data[key][key1]));
+                            sessionStorage.setItem(sSessionKey + '|' +  key1,JSON.stringify(data[key][key1]));
                         });
                     });
                 }
             }
         }
     }catch(exception){
+        console.log(exception);
     }
 }
 
@@ -160,7 +132,8 @@ var delRecords = function(event){
 
         var xhtmlreq = new XMLHttpRequest();
         var output = document.getElementById("out");
-        xhtmlreq.open("DELETE","https://flickering-fire-5311.firebaseio.com/" + sUserID + "/"+ sScenarioName + "/" + sLoanType + ".json");
+        // the window.btoa is a base64 converter.  This conversion is for the Firebase table IO.
+        xhtmlreq.open("DELETE","https://blistering-fire-7540.firebaseio.com/" + window.btoa(sUserID) + "/"+ window.btoa(sScenarioName) + "/" + sLoanType + ".json");
         xhtmlreq.send(null);
         xhtmlreq.onreadystatechange = function(){
             if(xhtmlreq.readyState == 4 && xhtmlreq.status == 200){
@@ -171,76 +144,8 @@ console.log("delete successful");
         }
         return true;
     }catch(exception){
+        console.log(exception);
         return false;
     }
 }
 
-var newScenario = function(event){
-    event.preventDefault();
-    document.getElementById("scenario-name").value = "";
-    document.getElementById("loan-type").value = ""; // car, home, other
-    document.getElementById("rate").value = "";
-    document.getElementById("principal").value = "";
-    document.getElementById("periods").value = "";
-    document.getElementById("period-type").value = ""; // month, quarter, year
-    document.getElementById("payment").value = "";
-    document.getElementById("total").value = "";
-    document.getElementById("interest-total").value = "";
-    
-}
-
-
-
-var init = function(event){
-    document.getElementById("add-records").addEventListener('click', saveScenario);
-    document.getElementById("delete-records").addEventListener('click', delRecords);
-    document.getElementById("get-all-records").addEventListener('click', getAllRecords);
-
-    document.getElementById("new").addEventListener('click', newScenario);
-    document.getElementById("calculate").addEventListener('click', Calculate);
-    
-    document.getElementById("principal").addEventListener('change', fmtPrinc);
-    document.getElementById("total").addEventListener('change', fmtTotal);
-    document.getElementById("interest-total").addEventListener('change', fmtTotInt);
-}
-
-// firebase encode/decoder
-var forbiddenChars = '.$[]#/'.split(''); //contains the firebase forbidden characters
-
-// function is designed to take in a simple string and then replace the offensive characters with 
-// %ascii_character%. It is designed to be called on any code that is sent to firebase.
-function firebaseEncode(str)
-{
-    var encodedStr = str;
-    
-    for (var i = 0; i<forbiddenChars.length; i++)
-    {
-        while ( encodedStr.indexOf(forbiddenChars[i]) != -1 ) {
-            encodedStr = encodedStr.replace(forbiddenChars[i], '%' + forbiddenChars[i].charCodeAt(0) + '%');
-        };
-        
-       // encodedStr.replace(new RegExp(forbiddenChars[i], 'g'), '%' + forbiddenChars[i].charCodeAt(0) + '%');
-    }
-    
-    return encodedStr;
-}
-
-// function is designed to take in a string that has been encoded with the matching encoder and reverse the string
-// back to its human readiable form.
-function firebaseDecode(encodedStr)
-{
-    var decodedStr = encodedStr;
-    
-    for (var i = 0; i<forbiddenChars.length; i++)
-    {
-        while ( decodedStr.indexOf('%' + forbiddenChars[i].charCodeAt(0) + '%') != -1 ) {
-            decodedStr = decodedStr.replace('%' + forbiddenChars[i].charCodeAt(0) + '%', forbiddenChars[i]);
-        };
-        
-       // encodedStr.replace(new RegExp(forbiddenChars[i], 'g'), '%' + forbiddenChars[i].charCodeAt(0) + '%');
-    }
-    
-    return decodedStr;
-}
-
-document.addEventListener("DOMContentLoaded",init);
--- js/utils.js
+++ js/utils.js
@@ -41,22 +41,15 @@ function GetPeriodIndicator(){
     return document.getElementById("period_type").value;
 }
 
-
-function NumberWithCommas(x) {
-    var parts = x.toString().split("."); // split the number on the decimal
+// insert commas for display to the form.
+function NumberWithCommas(sValue) {
+    var parts = sValue.toString().split("."); // split the number on the decimal
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ","); // insert the commas
     return parts.join("."); // reassemble the number on the decimal
 }
 
+// validate the input to conform to email format
 function ValidateEmail(mail){
-/*
-    if(typeof (mail) !== undefined && mail !== "" && mail !== null){
-        return true;
-    }else{
-        return false;
-    }
-*/
-    
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))  
     {  
         return true; 
@@ -64,14 +57,11 @@ function ValidateEmail(mail){
     return false;
 } 
 
-function isValid(str){
-    if(/^[a-zA-Z0-9- ]*$/.test(str) === false) {
-        return false;
-    }
-    return true;
-}
 
 
+
+
+// This function is the button handler for saving a scenario
 var saveScenario = function (event) {
     try{
         event.preventDefault();
@@ -94,7 +84,7 @@ var saveScenario = function (event) {
                     // else, validate the value to email address.
                     if(ValidateEmail(sUserID)){
 
-                        sUserID = sUserID.replace( /\./g, "" ); // strip out periods as firebase doesn't like them
+                        //sUserID = sUserID.replace( /\./g, "" ); // strip out periods as firebase doesn't like them
                         // save User ID to persistent local storage
                         localStorage.setItem('nerdherdcalc-userid', sUserID);
                         break;
@@ -111,13 +101,7 @@ var saveScenario = function (event) {
             alert("Scenario name cannot be blank.");
             return;
         }else{
-            if(!isValid(sScenarioName.value)){
-                addClass(sScenarioName, "inError");
-                alert("Valid Scenario names may contain only A to Z (upper and lower case), and 0-9.  Please re-enter the scenario name.");
-                return;
-            }else{
-                removeClass(sScenarioName, "inError");
-            }
+            removeClass(sScenarioName, "inError");
         }
         // attempt to add the records to firebase
 
@@ -176,3 +160,82 @@ console.log(event.target.textContent);
     }catch(exception){
     }
 }
+
+var newScenario = function(event){
+    event.preventDefault();
+    document.getElementById("scenario-name").value = "";
+    document.getElementById("loan-type").value = ""; // car, home, other
+    document.getElementById("rate").value = "";
+    document.getElementById("principal").value = "";
+    document.getElementById("periods").value = "";
+    document.getElementById("period-type").value = ""; // month, quarter, year
+    document.getElementById("payment").value = "";
+    document.getElementById("total").value = "";
+    document.getElementById("interest-total").value = "";
+    
+}
+
+
+// initialization function to load event listeners for static buttons and the like.
+var init = function(event){
+    document.getElementById("add-records").addEventListener('click', saveScenario);
+    document.getElementById("delete-records").addEventListener('click', delRecords);
+    document.getElementById("get-all-records").addEventListener('click', getAllRecords);
+
+    document.getElementById("new").addEventListener('click', newScenario);
+    document.getElementById("calculate").addEventListener('click', Calculate);
+    
+    document.getElementById("principal").addEventListener('change', fmtPrinc);
+    document.getElementById("total").addEventListener('change', fmtTotal);
+    document.getElementById("interest-total").addEventListener('change', fmtTotInt);
+}
+
+
+document.addEventListener("DOMContentLoaded",init);
+
+
+
+
+
+
+// the encoder and decoder might be abandoned
+
+// firebase encode/decoder
+var forbiddenChars = '.$[]#/'.split(''); //contains the firebase forbidden characters
+
+// function is designed to take in a simple string and then replace the offensive characters with 
+// %ascii_character%. It is designed to be called on any code that is sent to firebase.
+function firebaseEncode(str)
+{
+    var encodedStr = str;
+    
+    for (var i = 0; i<forbiddenChars.length; i++)
+    {
+        while ( encodedStr.indexOf(forbiddenChars[i]) != -1 ) {
+            encodedStr = encodedStr.replace(forbiddenChars[i], '%' + forbiddenChars[i].charCodeAt(0) + '%');
+        };
+        
+       // encodedStr.replace(new RegExp(forbiddenChars[i], 'g'), '%' + forbiddenChars[i].charCodeAt(0) + '%');
+    }
+    
+    return encodedStr;
+}
+
+// function is designed to take in a string that has been encoded with the matching encoder and reverse the string
+// back to its human readiable form.
+function firebaseDecode(encodedStr)
+{
+    var decodedStr = encodedStr;
+    
+    for (var i = 0; i<forbiddenChars.length; i++)
+    {
+        while ( decodedStr.indexOf('%' + forbiddenChars[i].charCodeAt(0) + '%') != -1 ) {
+            decodedStr = decodedStr.replace('%' + forbiddenChars[i].charCodeAt(0) + '%', forbiddenChars[i]);
+        };
+        
+       // encodedStr.replace(new RegExp(forbiddenChars[i], 'g'), '%' + forbiddenChars[i].charCodeAt(0) + '%');
+    }
+    
+    return decodedStr;
+}
+
