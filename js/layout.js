6d3b2b2e79b925e439ff67e079723cc69543f31d
charlesjackson261 <cmj.android@gmail.com>
2014-03-03 14:04:22 -0800

    Added function to remove and add scenarios on the fly

--- js/layout.js
+++ js/layout.js
@@ -8,6 +8,8 @@ var openModalID = null;
  */
 function showModal(winID) {
     
+    /*
+    
     // check to see if the element is valid.
     var win = document.getElementById(winID);
     if (!(typeof(win) != 'undefined' && win != null))
@@ -36,7 +38,7 @@ function showModal(winID) {
     win.classList.add('modal');
     
     openModalID = winID;
-    
+    */
 }
 
 
@@ -55,11 +57,125 @@ function hideModal() {
     
 }
 
+// the unique id will be returned from the listerner method
+function addScenario( iconURL, name, type, value, apr, uniqueID )
+{
+    // check to see if the scenario-list exists
+    var scenarioListWin = document.getElementById('scenario-list');
+    if (!(typeof(scenarioListWin) != 'undefined' && scenarioListWin != null))
+    {
+        // not exists.
+        alert("Selected scenario list cannot be found. (" + winID + ")" );
+        return;
+    }
+    
+    /*
+        <section class="scenario-item grLtBlu">
+            <img src="images/icons/cash.png" >
+            <div class="title">Computer Loan</div>
+            Other / $3,000.00 / 4.36%
+            <div class="btn-panel">
+                <button type="button" class="btn btn-danger">Delete</button>
+                <button type="button" class="btn btn-primary">Load</button>
+            </div>
+        </section>
+    */
+
+    newScenarioItem = document.createElement("section");
+    newScenarioItem.id = uniqueID;
+    newScenarioItem.classList.add("scenario-item");
+    newScenarioItem.classList.add("grLtBlu");
+    
+    // add the items to the scenario item
+    scenarioImg = document.createElement("img");
+    scenarioImg.id = uniqueID + "_img";
+    scenarioImg.setAttribute('src', 'images/icons/cash.png');
+    newScenarioItem.appendChild(scenarioImg);
+    
+    // add the title
+    scenarioTitle = document.createElement("div");
+    scenarioTitle.id = uniqueID + "_title";
+    scenarioTitle.classList.add('title');
+    scenarioTitle.innerHTML = name;
+    newScenarioItem.appendChild(scenarioTitle);
+    
+    // add the details.
+    newScenarioItem.innerHTML += type + ' / ' + value + ' / ' + apr;
+    
+    // add button panel
+    scenarioButtonPanel = document.createElement("div");
+    scenarioButtonPanel.id = uniqueID + "_btn_panel";
+    scenarioButtonPanel.classList.add('btn-panel');
+    
+    // add buttons
+        // add the button
+        scenarioDeleteButton = document.createElement("button");
+        scenarioDeleteButton.id = uniqueID + "_button";
+        scenarioDeleteButton.classList.add('btn');
+        scenarioDeleteButton.classList.add('btn-danger');
+        scenarioDeleteButton.innerHTML = 'Delete';
+        scenarioDeleteButton.setAttribute('type', 'button');
+        scenarioDeleteButton.addEventListener("click", function () { removeScenarioByElement( scenarioDeleteButton ) });
+        scenarioButtonPanel.appendChild(scenarioDeleteButton);
+
+        // add the button
+        scenarioLoadButton = document.createElement("button");
+        scenarioLoadButton.id = uniqueID + "_button";
+        scenarioLoadButton.classList.add('btn');
+        scenarioLoadButton.classList.add('btn-primary');
+        scenarioLoadButton.innerHTML = 'Load';
+        scenarioLoadButton.setAttribute('type', 'button');
+        scenarioButtonPanel.appendChild(scenarioLoadButton);
+    
+    newScenarioItem.appendChild(scenarioButtonPanel);
+
+    
+    
+    
+    scenarioListWin.appendChild(newScenarioItem);
+    
+}
+
+// the unique id will be returned from the listerner method
+function removeScenarioByID( uniqueID )
+{
+    // 
+}
+
+function removeScenarioByElement( btnElement )
+{
+    // find the parent scenario-item
+    selElement = btnElement.parentElement;
+    
+    // find the parent item
+    while (!selElement.classList.contains('scenario-item'))
+    {
+        selElement = selElement.parentElement;
+    }
+    
+    selElement.remove();
+}
+
+// utility
+
 function remove(id)
 {
     return (elem=document.getElementById(id)).parentNode.removeChild(elem);
 }
 
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+             .toString(16)
+             .substring(1);
+};
+
+function guid() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();
+}
+
 // Event listeners
-document.getElementById("open-load-screen").addEventListener("click", function () { showModal("load-screen") });
-document.getElementById("close-modal").addEventListener("click", function () { hideModal() });
+// document.getElementById("open-load-screen").addEventListener("click", function () { showModal("load-screen") });
+// document.getElementById("close-modal").addEventListener("click", function () { hideModal() });
+
+document.getElementById("btn-add-scenario").addEventListener("click", function () { addScenario("cash.png", "new Item", "Auto", "$3,000.00", "4.65%", guid() ) });
