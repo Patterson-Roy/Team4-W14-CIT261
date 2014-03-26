a3563fa7469ece9ced372c6f9ac43052f92218b4
Greg Hall <gvh574@gmail.com>
2014-03-25 19:38:33 -0600

    Implemented new formula's and method of calculation.

--- js/calculate.js
+++ js/calculate.js
@@ -1,142 +1,147 @@
 function Calculate () {
-    // Bring in values, convert from string to Float(int)
-    var rate = parseFloat(document.getElementById("rate").value.replace(/,/g, ""));
-    rate = rate/100;
-    var principal = parseFloat(document.getElementById("principal").value.replace(/,/g, ""));
-    var periods = parseFloat(document.getElementById("periods").value.replace(/,/g, ""));
-    var ppy = parseFloat(calcPeriodsPerYear());
-    var payment = parseFloat(document.getElementById("payment").value.replace(/,/g, ""));
-  
+    var principal = parseFloat(document.getElementById('principal').value.replace( /,/g, "" ));
+    var payment = parseFloat(document.getElementById('payment').value.replace( /,/g, "" ));
+    var rate = parseFloat(document.getElementById('rate').value.replace( /,/g, "" ));
+    var term = parseInt(document.getElementById('term').value.replace( /,/g, "" ));
+    var data = {};
+
+
         switch (this.whatToCalculate){
                 case "principal":
-                    CalcPrincipal(rate, periods, ppy, payment);
-                    return;
+                    data = CalcPrincipal(payment, rate, term);
+                    break;
                 
                 case "payment":
-                    CalcPayment(principal, rate, periods, ppy);
-                    return;
+                    data = CalcPayment(principal, rate, term);
+                    break;
                 
-                case "periods":
-                    CalcPeriods(principal, rate, ppy, payment);
-                    return;
+                case "term":
+                    data = CalcTerm(principal, rate, payment);
+                    break;
                 
                 case "rate":
-                    CalcRate(principal, payment, ppy, periods);
-                    return;
+                    data = CalcRate(principal, payment, term);
+                    break;
                 
                 default:
-                    return;
+                    break;
+        }
+        
+
+        document.getElementById("principal").value = NumberWithCommas(data.principal.toFixed(2));
+        document.getElementById("payment").value = NumberWithCommas(data.payment.toFixed(2));
+        document.getElementById("rate").value = NumberWithCommas(data.rate.toFixed(3));
+        document.getElementById("term").value = data.term;
+        
+        document.getElementById("total").value=NumberWithCommas(data.total.toFixed(2));
+        document.getElementById("interest-total").value=NumberWithCommas(data.totalInt.toFixed(2));
+
+        if(document.getElementById('principal').value != undefined && document.getElementById('principal').value > "0" &&
+                document.getElementById('payment').value != undefined && document.getElementById('payment').value >"0" &&
+                document.getElementById('rate').value != undefined && document.getElementById('rate').value > "0"){
+            
+            document.getElementById('loan-amt').textContent = document.getElementById('principal').value;
+            removeClass(document.getElementById("amortizeButton"), "hide-me");
         }
 }
 
-//principal = loan amount
-//rate = interest rate
-//periods = number of period.
-//ppy = periods/year
-
-function CalcPrincipal(rate, periods, ppy, payment) {
-    var totalPrincipal = (payment * (1- Math.pow((1+(rate/ppy)), -periods))) / (rate/ppy);
+function CalcPrincipal (payment, rate, term) {
+    var data = {};
+    var interest = rate * .01;
+    var period = getPeriodsPerYear();
+    var periodInterest = interest/period;
     
-    //Calculate principal plus interest giving loan total and loan interest total
+    data.principal = payment * (1-Math.pow((1 + periodInterest),(0-term)))/periodInterest;
+    data.rate = rate;
+    data.term = term;
+    data.payment = parseFloat(payment);
     
-    var loanTotal = payment * periods;
-    loanTotal = parseFloat(loanTotal);
-    var loanInt = loanTotal - totalPrincipal;
-    loanInt = parseFloat(loanInt);
+    data.total = payment * term;
+    data.totalInt = data.total - data.principal;
 
-    document.getElementById("principal").value = NumberWithCommas(totalPrincipal.toFixed(2));
-    // reformat the rate
-    document.getElementById('rate').value = NumberWithCommas(parseFloat(rate * 100).toFixed(3));
-    document.getElementById('total').value = NumberWithCommas(loanTotal.toFixed(2));
-    document.getElementById('interest-total').value = NumberWithCommas(loanInt.toFixed(2));
-    return;
+    return data;
 }
     
-function CalcPayment(principal, rate, periods, ppy) {
+function CalcPayment (principal, rate, term) {
+    var data = {};
+    var interest = rate * .01;
+    var period = getPeriodsPerYear();
+    var periodInterest = interest/period;
+     
+    //var payment = principal * periodInterest/denominator;
+    data.principal = principal;
+    data.rate = rate;
+    data.term = term;
+    data.payment = (principal * periodInterest * Math.pow((1+periodInterest),term))/(Math.pow((1+periodInterest),term)-1);
+    data.total = data.payment * term;
+    data.totalInt = data.total - principal;
     
-        // calculate the payment value
+    return data ;
     
-    var payment = parseFloat((principal*(rate/ppy)) / (1- Math.pow((1+(rate/ppy)), -periods)));
+}
 
-        //Calculate principal plus interest giving loan total and loan interest total
+function CalcTerm (principal, rate, payment) {
+    var data = {};
+    var interest = rate * .01;
+    var period = getPeriodsPerYear();
+    
+    var pt1 = Math.log(1 - (principal/payment * interest/period));
+    var pt2 = Math.log(1 + interest/period);
+    var term =  parseInt(Math.abs(pt1 / pt2) + .5);
+
+
+    data.principal = principal;
+    data.rate = rate;
+    data.term = term;
+    data.payment = payment;
+    data.total = payment * term;
+    data.totalInt = data.total - principal;
+
+    return data;
+}
+
+function CalcRate (principal, payment, term) {
+    var data = {};
+    var total = 0.0;
+ 
+/*
+ * approximation starting point
+i ≈ [ ( 1 + ( P/A ) ) ^ 1/q  - 1 ] ^q  - 1
+
+Where,
+ q = log[ 1 + (1/N) ] / log2 
+ P = Payment Amount
+ A = Initial Amount 
+ N = Number of Payments  
+*/
+    
+    var q = Math.log(1 + 1/term)/Math.LN2;
+    
+    var period = getPeriodsPerYear();
+    
+    var rate = ((Math.pow((Math.pow((1+payment/principal),(1/q))-1), q) -1)* 100 * period) + 1;
+    var calcPay = payment + 1;
+    
+    while(total < principal && calcPay > payment){
         
-    var loanTotal = payment * periods;
-    loanTotal = parseFloat(loanTotal);
-    var loanInt = loanTotal - principal;
-    loanInt = parseFloat(loanInt);
-    
-        // now set the decimal precision to avoid rounding errors
-    
-    document.getElementById("payment").value = NumberWithCommas(payment.toFixed(2));
+        rate -= .0001;
 
-    // reformat the principal
-    // string any commas first, then reformat for display
-    document.getElementById("principal").value = NumberWithCommas(parseFloat(principal).toFixed(2));
-    // reformat the rate
-    document.getElementById('rate').value = NumberWithCommas(parseFloat(rate * 100).toFixed(3));
-    
-    document.getElementById('total').value = NumberWithCommas(loanTotal.toFixed(2));
-    document.getElementById('interest-total').value = NumberWithCommas(loanInt.toFixed(2));
-    return;
-}
-
-function CalcPeriods(principal, rate, ppy, payment) {
-    
-//    var pt1 = Math.log(payment) - Math.log(payment - (principal*rate) / ppy) ;
-    var pt1 = Math.log(1 - (principal/payment * rate/ppy));
-    var pt2 = Math.log(1 + rate/ppy);
-    var periods =  parseInt(Math.abs(pt1 / pt2) + .5);
-//    periods = parseInt(periods + .5);
-    
-       //Calculate principal plus interest giving loan total and loan interest total
-    var loanTotal = payment * periods;
-    loanTotal = parseFloat(loanTotal);
-    var loanInt = loanTotal - principal;
-    loanInt = parseFloat(loanInt);
-    
-    document.getElementById("periods").value = periods;
-
-    // reformat the principal
-    // string any commas first, then reformat for display
-    document.getElementById("principal").value = NumberWithCommas(parseFloat(principal).toFixed(2));
-    // reformat the rate
-    document.getElementById('rate').value = NumberWithCommas(parseFloat(rate * 100).toFixed(3));
-    
-    document.getElementById('total').value = NumberWithCommas(loanTotal.toFixed(2));
-    document.getElementById('interest-total').value = NumberWithCommas(loanInt.toFixed(2));
-    return;
-}
-
-function CalcRate(principal, payment, ppy, periods) {
-    
-    var rate = 1.001;
-    var total = 0;
-    while(Math.abs(total)<principal){
-        rate -= .00001;
-        if(rate <= 0) break;
-        total = payment *((1-Math.pow(1+rate,-periods))/rate);
+        calcPay = CalcPayment(principal, rate, term).payment;
+        total = payment *((1-Math.pow(1+rate,-term))/rate);
     }
-    rate = (rate * 100 * ppy) + .005;
     
-        //Calculate principal plus interest giving loan total and loan interest total
-    var loanTotal = payment * periods;
-    loanTotal = parseFloat(loanTotal);
-    var loanInt = loanTotal - principal;
-    loanInt = parseFloat(loanInt);
+    data.principal = principal;
+    data.rate = rate;
+    data.payment = payment;
+    data.term = term;
     
-    document.getElementById("rate").value = NumberWithCommas(parseFloat(rate.toFixed(3)));
-
-    // reformat the principal
-    // string any commas first, then reformat for display
-    document.getElementById("principal").value = NumberWithCommas(parseFloat(principal).toFixed(2));
+    data.total = payment * term;
+    data.totalInt = data.total - principal;
     
-    document.getElementById('total').value = NumberWithCommas(loanTotal.toFixed(2));
-    document.getElementById('interest-total').value = NumberWithCommas(loanInt.toFixed(2));
-    return;
+    return data;
 }
-
     
-function calcPeriodsPerYear(){
+function getPeriodsPerYear(){
     var ppy = document.getElementById("period-type");
     var strPPY = ppy.options[ppy.selectedIndex].text;
         switch(strPPY){
